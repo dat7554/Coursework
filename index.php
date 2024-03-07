8e8cb7c26267c1f0f12e8986728722b63b79d31a
dat7554 <ndat7554@gmail.com>
2024-03-07 13:37:55 +0700

    link to profile

--- index.php
+++ index.php
@@ -11,21 +11,20 @@ include_once('common_function.php');
 <body>
 
 <?php
-
-//TODO: complete the link to profile
-echo '<a href="index.php">Home</a> | <a href="profile.php?id=$id">Profile</a>';
-if (@$_SESSION['email']) {
-
-    $sql = 'SELECT * FROM user';
+//header
+if (@$_SESSION['email']) { //check session
+    $email = $_SESSION['email'];
+    $sql = "SELECT userID FROM user WHERE email = :email";
     $statement = $pdo->prepare($sql);
+    $statement->bindParam(':email', $email, PDO::PARAM_STR);
     $statement->execute();
-    $rows = $statement->rowCount();
-    $row = $statement->fetch();
-    $id = $row['userID'];
+    $id = $statement->fetch()['userID'];
 
+    echo "<center><a href='index.php'>Home</a> | <a href='profile.php?id=$id'>Profile</a>";
+    echo "</center>";
     echo "<p>Welcome, <strong>" . @$_SESSION['email'] . "</strong> | <a href='index.php?action=sign_out'>Sign out</a></p>";
-} else {
-    echo "<a href='sign_in.php'>Sign in</a> | <a href='sign_up.php'>Sign up</a>";
+} else { //header public view
+    echo "<center><a href='index.php'>Home</a> | <a href='sign_in.php'>Sign in</a> | <a href='sign_up.php'>Sign up</a></center>";
 }
 ?>
 
