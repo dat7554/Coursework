1e106e7290a5383f5a62264251c471f080fb7fc1
dat7554 <ndat7554@gmail.com>
2024-03-11 20:05:25 +0700

    check and add user_role rows

--- Database/user.php
+++ Database/user.php
@@ -1,17 +1,22 @@
 <?php
+//user_roleID currently fk in user, should consider user_roleID out of user (userID fk of user_role) or not ?
+//later is easy to add new permission
+
 include_once('config.php');
 include_once('../connection.php');
 
 try {
     $user_sql = "CREATE TABLE IF NOT EXISTS user (
     userID INT AUTO_INCREMENT PRIMARY KEY,
+    user_roleID INT NOT NULL,
     email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     personal_description VARCHAR(1000) NOT NULL,
     active BOOLEAN DEFAULT true,
     log_in DATETIME NOT NULL,
-    register_date DATETIME DEFAULT CURRENT_TIMESTAMP)";
+    register_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_roleID) REFERENCES user_role(user_roleID))";
 
     $pdo->exec($user_sql); // Execute the SQL statement to create the table
     echo "User table created successfully!";
