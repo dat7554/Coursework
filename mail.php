2a11e8e2705d5a47ddd26fa07b8ab79477a2a4ca
dat7554 <ndat7554@gmail.com>
2024-04-16 18:49:05 +0700

    mail.php: applying phpmailer, install phpmailer

--- mail.php
+++ mail.php
@@ -1,4 +1,14 @@
 <?php
+//code adapted from: https://github.com/PHPMailer/PHPMailer
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 //check user
 if (!isset($_SESSION['email'])) {
     exit();
@@ -12,10 +22,41 @@ if (isset($_POST['btn_submit'])) {
     $subject = "Contact Form";
     $message = "Name: " . $first_name . " " . $last_name . "\nMessage: " . "\n\n" . $_POST['textarea_message'];
 
-    if (mail($to, $subject, $message)) {
-        echo "Mail sent successfully";
-    } else {
-        echo "Error";
+    if (empty($first_name) or empty($last_name) or empty($message)) {
+        echo "Please fill in all required fields";
+        exit();
+    }
+
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+        $mail->Username = 'nhpd7554@gmail.com';                     //SMTP username
+        $mail->Password = 'oruc bvyu fgxu znxr';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+        $mail->Port = 587;                                    //TCP port to connect to
+
+        //Recipients
+        $mail->setFrom($email, $last_name);
+        $mail->addAddress('ndat7554@gmail.com');
+        $mail->addReplyTo($email, 'User contact');
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+        $mail->AltBody = $message;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 ?>
